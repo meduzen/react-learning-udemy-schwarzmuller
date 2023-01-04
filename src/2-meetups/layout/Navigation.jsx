9ba2f2e5761fa2fd822d36c15262f8cecc439485
Mehdi M <hi@mehdi.cc>
2023-01-04 14:45:16 +0100

    Show favorite meetups

--- src/2-meetups/layout/Navigation.jsx
+++ src/2-meetups/layout/Navigation.jsx
@@ -1,16 +1,23 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import FavoritesContext from '../store/favorites'
+
 import styles from  './navigation.module.css'
 
-const MainNavigation = () => (
-  <header>
-      <nav>
-          <ul className={styles.ul}>
-            <li className={styles.li}><Link to='/meetups'>All meetups</Link></li>
-            <li className={styles.li}><Link to='/meetups/new'>New</Link></li>
-            <li className={styles.li}><Link to='/meetups/favorites'>Favorites</Link></li>
-          </ul>
-      </nav>
-  </header>
-)
+const MainNavigation = () => {
+  const favorites = useContext(FavoritesContext)
+
+  return (
+    <header>
+        <nav>
+            <ul className={styles.ul}>
+              <li className={styles.li}><Link to='/meetups'>All meetups</Link></li>
+              <li className={styles.li}><Link to='/meetups/new'>New</Link></li>
+              <li className={styles.li}><Link to='/meetups/favorites'>Favorites ({favorites.count})</Link></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
 
 export default MainNavigation
--- src/2-meetups/pages/Favorites.jsx
+++ src/2-meetups/pages/Favorites.jsx
@@ -1,7 +1,29 @@
-const Favorites = () => (
-  <div>
-      <h3>Favorites</h3>
-  </div>
-)
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import FavoritesContext from '../store/favorites'
+
+import MeetupsList from '../components/MeetupsList'
+
+const Favorites = () => {
+  const favorites = useContext(FavoritesContext)
+
+  // display texts
+
+  const countDisplay = favorites.count > 1 ? favorites.count : ''
+  const pluralizeMeetup = `meetup${favorites.count != 1 ? 's': ''}`
+
+  let title = favorites.count
+    ? <h3>Your {countDisplay} favorite {pluralizeMeetup}</h3>
+    : <h3>You have no favorite meetup, yet. <Link to={'/meetups'}>Go pick some</Link>!</h3>
+
+  return (
+    <div>
+      {title}
+      {!!favorites.count &&
+        <MeetupsList meetups={favorites.favorites}/>
+      }
+    </div>
+  )
+}
 
 export default Favorites
